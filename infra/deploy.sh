a05c9b71e84c49d6d8881df650dbe21d41eeef0c
Emil Broman <emil@emilbroman.me>
2019-11-19 14:39:51 +0100

    CI

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env sh
 
 SLUG=$1
+NAMESPACE=loalang
 
 if [ -z "$SLUG" ]; then
   RELEASE_NAME="loalang"
 else
   RELEASE_NAME="loalang-$SLUG"
+  NAMESPACE=loalang-staging
 fi
 
 helm upgrade \
   --set=global.slug="$SLUG" \
   --set=www.image.tag=$(find services/www -type f -exec md5sum {} \; | md5sum | awk '{print $1}') \
   --set=api.image.tag=$(find services/api -type f -exec md5sum {} \; | md5sum | awk '{print $1}') \
+  --namespace=$NAMESPACE \
   --install \
   "$RELEASE_NAME" \
   ./infra/k8s
