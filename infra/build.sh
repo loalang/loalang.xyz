bd972cb337a524300b8c8599579ee9192fd801c9
Emil Broman <emil@emilbroman.me>
2019-11-26 15:27:05 +0100

    Fix

--- infra/build.sh
+++ infra/build.sh
@@ -8,35 +8,37 @@ build() {
 
     VERSION="$(sh ./infra/id-of.sh services/$SERVICE)"
 
-    # Pull latest builder version
-    docker pull "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest" || true
+    docker pull "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:$VERSION" || {
+        # Pull latest builder version
+        docker pull "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest" || true
 
-    # Build new builder
-    docker build \
-        --cache-from "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest" \
-        -t "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest" \
-        -f infra/docker/services/$SERVICE/builder.dockerfile .
+        # Build new builder
+        docker build \
+            --cache-from "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest" \
+            -t "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest" \
+            -f infra/docker/services/$SERVICE/builder.dockerfile .
 
-    # Push new builder
-    docker push "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest"
+        # Push new builder
+        docker push "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest"
 
-    # Make local tag of builder for use in app image
-    docker tag "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest" $SERVICE-builder
+        # Make local tag of builder for use in app image
+        docker tag "registry.gitlab.com/loalang/loalang.xyz/$SERVICE-builder:latest" $SERVICE-builder
 
 
-    # Pull latest app version
-    docker pull "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:latest" || true
+        # Pull latest app version
+        docker pull "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:latest" || true
 
-    # Build new app
-    docker build \
-        --cache-from "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:latest" \
-        -t "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:latest" \
-        -t "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:$VERSION" \
-        -f infra/docker/services/$SERVICE/app.dockerfile .
+        # Build new app
+        docker build \
+            --cache-from "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:latest" \
+            -t "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:latest" \
+            -t "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:$VERSION" \
+            -f infra/docker/services/$SERVICE/app.dockerfile .
 
-    # Push new version
-    docker push "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:latest"
-    docker push "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:$VERSION"
+        # Push new version
+        docker push "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:latest"
+        docker push "registry.gitlab.com/loalang/loalang.xyz/$SERVICE:$VERSION"
+    }
 }
 
 build pkg
