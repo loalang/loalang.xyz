601344a610af637b9104387881555d8c2eabe3c8
Emil Broman <emil@emilbroman.me>
2019-11-28 09:59:45 +0100

    Enable package manager publish

--- services/api/src/PackageManager/PackageManager.ts
+++ services/api/src/PackageManager/PackageManager.ts
@@ -1,4 +1,7 @@
 import PackageManagerPackage from "./PackageManagerPackage";
+import { Readable } from "stream";
+import Package from "../Resolvers/Package";
+import http, { IncomingMessage } from "http";
 
 export default class PackageManager {
   constructor(private _host: string) {}
@@ -17,4 +20,42 @@ export default class PackageManager {
     }
     return new PackageManagerPackage(await response.json());
   }
+
+  async publish(
+    name: string,
+    version: string,
+    stream: Readable
+  ): Promise<Package> {
+    const response = await new Promise<IncomingMessage>((resolve, reject) => {
+      const request = http.request(
+        `${this._host}/packages/${name}?version=${version}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/tar+gzip"
+          }
+        }
+      );
+      stream.pipe(request);
+      request.once("response", resolve).once("error", reject);
+    });
+
+    if (response.statusCode !== 201) {
+      throw new Error(await collectString(response));
+    }
+
+    return collectString(response)
+      .then(JSON.parse)
+      .then(r => new PackageManagerPackage(r.pkg));
+  }
+}
+
+function collectString(stream: Readable): Promise<string> {
+  return new Promise((resolve, reject) => {
+    let buffer = "";
+    stream
+      .on("data", c => (buffer += c.toString("utf-8")))
+      .once("error", reject)
+      .once("end", () => resolve(buffer));
+  });
 }
--- services/api/src/Resolvers/Package.resolver.ts
+++ services/api/src/Resolvers/Package.resolver.ts
@@ -1,14 +1,29 @@
-import PackageManager from "../PackageManager/PackageManager";
 import Package from "./Package";
+import { FileUpload } from "graphql-upload";
+import Context from "../Context";
 
 export default {
   Query: {
     async package(
       _: any,
       { name }: { name: string },
-      { pkg }: { pkg: PackageManager }
+      { pkg }: Context
     ): Promise<Package | null> {
       return pkg.find(name);
+    }
+  },
+
+  Mutation: {
+    async publishPackage(
+      _: any,
+      {
+        name,
+        version,
+        package: upload
+      }: { name: string; version: string; package: Promise<FileUpload> },
+      { pkg }: Context
+    ): Promise<Package | null> {
+      return pkg.publish(name, version, (await upload).createReadStream());
     }
   }
 };
