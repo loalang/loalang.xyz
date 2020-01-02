f0de441a10c302262899ed4edd101dd9d7b16347
Emil Broman <emil@emilbroman.me>
2020-01-02 16:57:03 +0100

    Add notebook blocks

--- services/www/src/Views/NotebookView.tsx
+++ services/www/src/Views/NotebookView.tsx
@@ -4,7 +4,9 @@ import {
   useNotebook,
   useDeleteNotebook,
   Notebook,
-  usePublishNotebook
+  usePublishNotebook,
+  CodeNotebookBlock,
+  NotebookBlock
 } from "../Hooks/useNotebooks";
 import { useRouteMatch, useHistory } from "react-router-dom";
 import { NotFoundView } from "./NotFoundView";
@@ -13,9 +15,12 @@ import { Heading } from "@loalang/ui-toolbox/Typography/Heading";
 import { PageHeading } from "@loalang/ui-toolbox/Typography/TextStyle/PageHeading";
 import { Button } from "@loalang/ui-toolbox/Forms/Button";
 import { Form } from "@loalang/ui-toolbox/Forms/Form";
+import { TextInput } from "@loalang/ui-toolbox/Forms/TextInput";
 import { EditableText } from "../Components/EditableText";
 import { useTimeout } from "../Hooks/useTimeout";
 import { useIsOffline } from "../Hooks/useIsOffline";
+import uuid from "uuid/v4";
+import { Icon } from "@loalang/ui-toolbox/Icons/Icon";
 
 export function NotebookView() {
   const {
@@ -76,9 +81,75 @@ export function NotebookView() {
           >
             Delete
           </Button>
+
+          <Form.Array<Notebook, "blocks"> field="blocks">
+            {({ items, insert, unshift, push }) => (
+              <>
+                <CreateBlockButton onCreate={unshift} />
+                <ol>
+                  {items.map(({ value, onChange }, index) => {
+                    const addBetween =
+                      index === 0 ? null : (
+                        <CreateBlockButton
+                          onCreate={block => insert(index, block)}
+                        />
+                      );
+
+                    switch (value.__typename) {
+                      case "CodeNotebookBlock":
+                        return (
+                          <li key={value.id}>
+                            {addBetween}
+                            <Form value={value} onChange={onChange}>
+                              <Form.Input<
+                                CodeNotebookBlock,
+                                "code"
+                              > field="code">
+                                {({ value, onChange }) => (
+                                  <TextInput
+                                    value={value}
+                                    onChange={onChange}
+                                  />
+                                )}
+                              </Form.Input>
+                            </Form>
+                          </li>
+                        );
+
+                      default:
+                        return null;
+                    }
+                  })}
+                </ol>
+                <CreateBlockButton onCreate={push} />
+              </>
+            )}
+          </Form.Array>
+
           <Code>{JSON.stringify(notebook, null, 2)}</Code>
         </Form>
       )}
     </>
+  );
+}
+
+function CreateBlockButton({
+  onCreate
+}: {
+  onCreate: (block: NotebookBlock) => void;
+}) {
+  return (
+    <Button
+      onClick={() =>
+        onCreate({
+          __typename: "CodeNotebookBlock",
+          id: uuid(),
+          code: ""
+        })
+      }
+    >
+      <Icon.Edit />
+      Create Block
+    </Button>
   );
 }
