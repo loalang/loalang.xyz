864981235c7ee2dd4f307ab42b95d665ef4dc3a6
Emil Broman <emil@emilbroman.me>
2020-04-17 12:08:27 +0200

    Optimize web

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,15 +1,37 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
+const config = {
   module: {
     rules: [{
       test: /\.elm$/,
       exclude: [/elm-stuff/, /node_modules/],
       use: {
-        loader: 'elm-webpack-loader',
-        options: {}
-      }
-    }]
+        loader: "elm-webpack-loader",
+        options: {},
+      },
+    }],
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
+
+module.exports = (env, argv) => {
+  switch (argv.mode) {
+    case "production":
+      config.mode = "production";
+      config.output = {
+        filename: "[hash].js",
+      };
+      break;
+    case "development":
+    case undefined:
+      config.mode = "development";
+      break;
+    default:
+      throw new Error(`Invalid mode: ${argv.mode}`);
+  }
+  return config;
 };
